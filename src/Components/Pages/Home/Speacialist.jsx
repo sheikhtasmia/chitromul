bbc0d0def33b78f89c3b73df5c7a7148a51e3487
Sheikh Tasmia <200295689+sheikhtasmia@users.noreply.github.com>
2026-02-04 20:16:08 +0600

    Update Speacialist.jsx

--- src/Components/Pages/Home/Speacialist.jsx
+++ src/Components/Pages/Home/Speacialist.jsx
@@ -76,7 +76,7 @@ const Specialist = () => {
                 whileHover="hover"
               >
                 <motion.img
-                  src="/specialist/item_01.webp"
+                  src="/specialist/wedd2.webp"
                   alt="Wedding & Events Photography"
                   className="w-full h-full object-cover"
                   whileHover={{ scale: 1.1 }}
