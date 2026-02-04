1513c26d5d6e2c4b57b4dbb80187afaa03c05a58
Sheikh Tasmia <200295689+sheikhtasmia@users.noreply.github.com>
2026-02-04 20:13:36 +0600

    Update outdoor package image imports

--- src/Components/Pages/Home/PackagesSection.jsx
+++ src/Components/Pages/Home/PackagesSection.jsx
@@ -15,9 +15,9 @@ import premium4 from "../../../../public/Packages Premium/11.webp"
 
 
 // outdoor packages
-import outdoorPackages1 from "../../../../public/Packages Outdoor/1.webp"
-import outdoorPackages2 from "../../../../public/Packages Outdoor/2.webp"
-import outdoorPackages3 from "../../../../public/Packages Outdoor/3.webp"
+import outdoorPackages1 from "../../../../public/package-outdoor/1.webp"
+import outdoorPackages2 from "../../../../public/package-outdoor/2.webp"
+import outdoorPackages3 from "../../../../public/package-outdoor/3.webp"
 
 const regularImages = [regular1, regular2,regular3, regular4,];
 const premiumImages = [premium1, premium2,premium3, premium4,];
